d4e669ce88adc576749980d1b2c62106edf727fc
Funzyy <103183666+Funzyy@users.noreply.github.com>
2024-05-01 00:24:28 +0200

    update Header

--- header/WipImgui.h
+++ header/WipImgui.h
@@ -1,13 +1,8 @@
 #pragma once
-#ifndef IMGUI_INIT_H
-#define IMGUI_INIT_H
 
 #include "imgui.h"
 #include "imgui_impl_win32.h"
 #include "imgui_impl_dx12.h"
-
-#include <Window.h>
-#include <GaussianRenderer.h>
 
 int initImgui(ID3D12Device* device, int frameBufferCount, HWND hWnd);
 
@@ -16,5 +11,3 @@ int startMainImgui();
 int endMainImgui();
 
 int killImgui();
-
-#endif // IMGUI_INIT_H
