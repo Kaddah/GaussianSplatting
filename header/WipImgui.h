25c24c6c132d7ef6909e0718ce084b3d660b0d84
Funzyy <103183666+Funzyy@users.noreply.github.com>
2024-05-01 09:39:39 +0200

    OOF

--- header/WipImgui.h
+++ header/WipImgui.h
@@ -4,10 +4,10 @@
 #include "imgui_impl_win32.h"
 #include "imgui_impl_dx12.h"
 
-int initImgui(ID3D12Device* device, int frameBufferCount, HWND hWnd);
+void initImgui(ID3D12Device* device, int frameBufferCount, HWND hWnd);
 
-int startMainImgui();
+void startMainImgui();
 
-int endMainImgui();
+void endMainImgui();
 
-int killImgui();
+void killImgui();
