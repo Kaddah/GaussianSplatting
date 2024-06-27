38aabf611099c96b2b4259f28599616d02d7337e
Cretella, Marco <marco.cretella@stud.hs-coburg.de>
2024-06-27 14:13:46 +0200

    Revert "Tried to implement resizing, Stuck in WaitForPreviousFrame() method at: frameIndex = swapChain->GetCurrentBackBufferIndex();"
    This reverts commit 8338a88f622563a0a7ab9b847e9cb53206d3c2d6.

--- header/Window.h
+++ header/Window.h
@@ -33,7 +33,6 @@ public:
 
   void UpdateVertexBuffer(const std::vector<Vertex>& vertices);
   bool InitializeVertexBuffer(const std::vector<Vertex>& vertices);
-  void Resize(UINT newWidth, UINT newHeight);
   
   ~Window();
 
