cec9b352e51808aae0a1f288f855c1ff9d56cf04
Cretella, Marco <marco.cretella@stud.hs-coburg.de>
2024-08-13 16:47:09 +0200

    Added console output for dxexception to copy message

--- src/DxException.cpp
+++ src/DxException.cpp
@@ -1,4 +1,5 @@
 #include "DxException.h"
+#include <iostream>
 
 std::wstring AnsiToWString(const std::string& str)
 {
@@ -16,11 +17,21 @@ DxException::DxException(HRESULT hr, const std::wstring& functionName, const std
     , Filename(filename)
     , LineNumber(lineNumber)
 {
+  // Create the error message for console output
+  std::string errorMsg = "Error in: " + std::string(FunctionName.begin(), FunctionName.end()) + "\n" + 
+                         "in file: " + std::string(Filename.begin(), Filename.end()) + "\n" +
+                         "at line: " + std::to_string(LineNumber) + "\n" + 
+                         "with error code: " + std::to_string(hr);
+
+  // Print the error message to the console
+  std::cerr << errorMsg << std::endl;
 }
 
 const char* DxException::what() const noexcept
 {
-  whatBuffer = "Error in " + std::string(FunctionName.begin(), FunctionName.end()) + " in file " +
-               std::string(Filename.begin(), Filename.end()) + " at line " + std::to_string(LineNumber);
+  whatBuffer = "Error in line: " + std::string(FunctionName.begin(), FunctionName.end()) + "\n" + 
+               "in file: " + std::string(Filename.begin(), Filename.end()) + "\n" + 
+               "at line: " + std::to_string(LineNumber) + "\n" +
+               "with error code : " + std::to_string(ErrorCode);
   return whatBuffer.c_str();
 }
